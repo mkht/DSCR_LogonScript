c499e6134b924425e514263c7b93d30374407308
mkht <mkht@users.noreply.github.com>
2019-04-19 00:06:44 +0900

    Correct a file name pscripts.ini -> psscripts.ini

--- DSCResources/LogonScript/LogonScript.schema.psm1
+++ DSCResources/LogonScript/LogonScript.schema.psm1
@@ -41,8 +41,8 @@ Configuration LogonScript
     $GroupPolicyPath = 'C:\Windows\System32\GroupPolicy'
     $GptIniPath = Join-Path -Path $GroupPolicyPath -ChildPath 'gpt.ini'
 
-    $UserScriptsIniPath = Join-Path -Path $GroupPolicyPath -ChildPath ('\User\Scripts\{0}scripts.ini' -f $(if ($ScriptType -eq 'PowerShell') { 'p' }))
-    $MachineScriptsIniPath = Join-Path -Path $GroupPolicyPath -ChildPath ('\Machine\Scripts\{0}scripts.ini' -f $(if ($ScriptType -eq 'PowerShell') { 'p' }))
+    $UserScriptsIniPath = Join-Path -Path $GroupPolicyPath -ChildPath ('\User\Scripts\{0}scripts.ini' -f $(if ($ScriptType -eq 'PowerShell') { 'ps' }))
+    $MachineScriptsIniPath = Join-Path -Path $GroupPolicyPath -ChildPath ('\Machine\Scripts\{0}scripts.ini' -f $(if ($ScriptType -eq 'PowerShell') { 'ps' }))
 
     $UserScriptCSE = '{42B5FAAE-6536-11D2-AE5A-0000F87571E3}{40B66650-4972-11D1-A7CA-0000F87571E3}'
     $MachineScriptCSE = '{42B5FAAE-6536-11D2-AE5A-0000F87571E3}{40B6664F-4972-11D1-A7CA-0000F87571E3}'
